90bd3c9056eafdc78c0b87b6ddba2c763fa85b88
Lars Willighagen <lars.willighagen@gmail.com>
2018-10-15 10:38:23 +0200

    fix(polygon): improve direction algorithm
    Improve algorithm determining the direction of a polygon. The new one 
    should handle all edge cases, except for polygons that don't have a 
    defined direction of course.

--- src/normalizePolygonDirection.js
+++ src/normalizePolygonDirection.js
@@ -1,18 +1,32 @@
 export function normalizePolygonDirection (polygon) {
   let xmax = -Infinity
+  let ymax
   let xindex = -1
 
-  for (let [i, [x]] of Object.entries(polygon)) {
+  for (let [i, [x, y]] of Object.entries(polygon)) {
     if (x > xmax) {
       xmax = x
+      ymax = y
       xindex = +i
     }
   }
 
-  let prev = polygon[(polygon.length + xindex - 1) % polygon.length]
-  let next = polygon[(polygon.length + xindex + 1) % polygon.length]
+  let clockwise
+  for (let i = 1; clockwise === undefined && i < (polygon.length + 1) / 2; i++) {
+    let prev = polygon[(polygon.length + xindex - i) % polygon.length]
+    let next = polygon[(polygon.length + xindex + i) % polygon.length]
 
-  let clockwise = prev[1] > next[1]
+    if (prev[1] !== next[1]) {
+      clockwise = prev[1] < next[1]
+    } else if (prev[0] !== next[0] && prev[1] !== ymax) {
+      clockwise = (prev[1] < ymax) === (prev[0] > next[0])
+    } else {
+      continue
+    }
+  }
+
+  // clockwise is only undefined when the polygon is just a polyline,
+  // in which case the direction *is* undefined
 
   if (clockwise) {
     return polygon
