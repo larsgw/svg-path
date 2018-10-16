4ad4c2ea6357cc9f7823d7f9bf6b65b5d7280e96
Lars Willighagen <lars.willighagen@gmail.com>
2018-10-16 13:45:51 +0200

    chore(babel): add pipeline operator

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
-    ['@babel/env', {targets: {node: '6'}}]
+    ['@babel/env', {targets: {node: '6'}}],
+  ],
+  plugins: [
+    ['@babel/proposal-pipeline-operator', {proposal: 'minimal'}]
   ],
   comments: false
 }
