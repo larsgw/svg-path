d94481ca3d19c7ad0eca5a630a69946dea8679f9
Lars Willighagen <lars.willighagen@gmail.com>
2018-10-22 22:26:28 +0200

    fix(polygon): improve handling of edge cases
    Improve handling of certain edge cases:
      - point-line intersections
      - point-point intersections
      - any intersections with coincident segments
      - near misses (as in, any of the above that
        don't turn out to be intersections, but just
        "bounce" back
    
    Still missing one test case (with the possibility
    for more) relating to the handling of said
    intersections.

--- src/normalizePolygon.js
+++ src/normalizePolygon.js
@@ -1,4 +1,4 @@
-function isOnLineSegment ([x, y], a, b) {
+function isBetweenOrOnPoints ([x, y], a, b) {
   let xmin = Math.min(a[0], b[0])
   let xmax = Math.max(a[0], b[0])
   let ymin = Math.min(a[1], b[1])
@@ -7,7 +7,48 @@ function isOnLineSegment ([x, y], a, b) {
   return x >= xmin && x <= xmax && y >= ymin && y <= ymax
 }
 
-function getIntersection ([xc, yc], [xd, yd], cache) {
+function isBetweenPoints (c, a, b) {
+  let isOneOfPoints = isSamePoint(c, a) || isSamePoint(c, b)
+  return isBetweenOrOnPoints(c, a, b) && !isOneOfPoints
+}
+
+function isSamePoint ([xa, ya], [xb, yb]) {
+  return xa === xb && ya === yb
+}
+
+function isToLeftOfLine ([intersection, a, c], polygon) {
+  let b = a + 1
+  let d = c + 1
+  let [cx, cy] = polygon[isSamePoint(intersection, polygon[d]) ? c : d] // TODO: assumption, watch out
+  let [ax, ay] = polygon[isSamePoint(intersection, polygon[a]) ? a - 1 : a]
+  let [bx, by] = polygon[isSamePoint(intersection, polygon[b]) ? b + 1 : b]
+  let [sx, sy] = intersection
+
+  let alpha = Math.atan2(ay - sy, ax - sx)
+  let beta = Math.atan2(by - sy, bx - sx)
+  let gamma = Math.atan2(cy - sy, cx - sx)
+
+  beta -= alpha
+  gamma -= alpha
+
+  if (beta < 0) { beta += 2 * Math.PI }
+  if (gamma < 0) { gamma += 2 * Math.PI }
+
+  return gamma === beta ? undefined : gamma < beta
+}
+
+function isActualIntersection (entry, exit, polygon) {
+  let entryData = isToLeftOfLine(entry, polygon)
+  let exitData = isToLeftOfLine(exit, polygon)
+
+  if (entryData === undefined || exitData === undefined) {
+    return undefined
+  }
+
+  return entryData !== exitData
+}
+
+function getCachedIntersection ([xc, yc], [xd, yd], cache) {
   let p = xc * cache[0] + yc * cache[1] + cache[2]
   let q = xd * cache[0] + yd * cache[1] + cache[2]
   let r = p / (p - q)
@@ -22,61 +63,82 @@ function getIntersection ([xc, yc], [xd, yd], cache) {
   ]
 }
 
+function getIntersectionCache ([xa, ya], [xb, yb]) {
+  return [
+    yb - ya,
+    xa - xb,
+    ya * xb - xa * yb
+  ]
+}
+
+function getIntersection (a, b, c, d) {
+  return getCachedIntersection(c, d, getIntersectionCache(a, b))
+}
+
 export function getPolygonIntersections (polygon) {
   // line segments are indexed by the first point in the polygon that it's part of
   // intersection = [[x, y], segmentA, segmentB]
   let intersections = []
+  // indefinite intersections are those that are only the entry part, the exit still
+  // has to be determined
+  let indefinite
+  // cache part of intersection calculations. due to the simplicity of the calculations
+  // it probably has a negative effect
   let caches = []
 
   // a = 1st point of 1st line segment, b = 2nd point of 1st line segment,
   // c = 1st point of 2nd line segment, d = 2nd point of 2nd line segment
   for (let c = 0; c < polygon.length - 1; c++) {
     let d = c + 1
-    let [xc, yc] = polygon[c]
-    let [xd, yd] = polygon[d]
-    caches.push([
-      yd - yc,
-      xc - xd,
-      yc * xd - xc * yd
-    ])
+    caches.push(getIntersectionCache(polygon[c], polygon[d]))
 
     for (let a = 0; a < c - 1; a++) {
       let b = a + 1
-      let intersection = getIntersection([xc, yc], [xd, yd], caches[a])
+      let intersection = getCachedIntersection(polygon[c], polygon[d], caches[a])
 
-      // no intersection
-      if (intersection === false) {
+      // no or infinite intersection
+      if (intersection === false || intersection === true) {
         continue
       }
 
-      // infinite intersections
-      if (intersection === true) {
-        if (isOnLineSegment(polygon[c], polygon[a], polygon[b])) {
-          intersections.push([polygon[c], a, c])
-        } else if (isOnLineSegment(polygon[d], polygon[a], polygon[c])) {
-          intersections.push([polygon[d], a, c])
+      // intersection is between begin and endpoint of polygon, and polygon
+      // begins and ends in the same point
+      // that's the same as line segments intersecting with the previous
+      // segment, which is also ignored by looping until c - 1 instead of c
+      if (a === 0 && c === polygon.length - 2 &&
+          isSamePoint(intersection, polygon[a]) &&
+          isSamePoint(intersection, polygon[d])) {
+        continue
+      }
+
+      // intersection with line but not line segments
+      if (!isBetweenOrOnPoints(intersection, polygon[a], polygon[b]) ||
+          !isBetweenOrOnPoints(intersection, polygon[c], polygon[d])) {
+        continue
+      }
+
+      // no definitive intersection
+      if ((isSamePoint(intersection, polygon[a]) && isSamePoint(polygon[a], polygon[c])) ||
+          (isSamePoint(intersection, polygon[a]) && isSamePoint(polygon[a], polygon[d]))) {
+        continue
+      } else if (!isBetweenPoints(intersection, polygon[a], polygon[b]) ||
+                 !isBetweenPoints(intersection, polygon[c], polygon[d])) {
+        if (indefinite) {
+          let isIntersection = isActualIntersection(indefinite, [intersection, a, c], polygon)
+          if (isIntersection === undefined) {
+            // parallel line, no exit yet
+          } else if (isIntersection) {
+            intersections.push(indefinite)
+            indefinite = undefined
+          } else {
+            indefinite = undefined
+          }
         } else {
-          // line segments don't overlap
+          indefinite = [intersection, a, c, 'a']
         }
-        continue
+      } else {
+        intersections.push([intersection, a, c, 'b'])
       }
-
-      let [xi, yi] = intersection
-
-      // intersection with line but not line segments & intersection at endpoint
-      let rightBound = Math.min(Math.max(polygon[a][0], polygon[a][1]), Math.max(xc, xd))
-      let leftBound = Math.max(Math.min(polygon[a][0], polygon[a][1]), Math.min(xc, xd))
-      if (xi <= leftBound || xi >= rightBound) {
-        continue
-      }
-
-      // intersection is between begin and endpoint of polygon, and
-      // polygon begins and ends in the same point
-      if (a === 0 && c === polygon.length - 2 && polygon[c][0] === polygon[a][0] && polygon[c][1] === polygon[a][1]) {
-        continue
-      }
-
-      intersections.push([[xi, yi], a, c])
     }
   }
 
